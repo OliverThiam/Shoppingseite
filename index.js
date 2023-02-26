74f755208868a5228e26ecc875a620469c505e7b
Laminethrdream <oliver.laminethiam@gmail.com>
2023-02-26 17:41:42 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,5 @@
+let btnEl = document.getElementById("btn-el")
+
+btnEl.addEventListener("click", function(){
+    window.location.replace("https://stackoverflow.com/questions/57536070/how-to-make-button-open-a-link-in-new-tab-in-javascript");
+})
